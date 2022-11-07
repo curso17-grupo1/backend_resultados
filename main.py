48964a6a3cf725d609092cce7315de08eb3c448e
Jonathan Avila <jonandresavila@hotmail.com>
2022-11-07 01:36:14 -0500

    Main Actualizado y funcional con pruebas sobre postman

--- main.py
+++ main.py
@@ -1,16 +1,38 @@
-# This is a sample Python script.
+import json
 
-# Press Mayús+F10 to execute it or replace it with your code.
-# Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
+from flask import Flask
+from flask import jsonify
+from flask_cors import CORS
+from waitress import serve
+
+from blueprints.candidato_blueprint import candidato_blueprint
+from blueprints.mesa_blueprint import mesa_blueprint
+from blueprints.partido_blueprint import partido_blueprint
+from blueprints.resultado_blueprint import resultado_blueprint
+
+app = Flask(__name__)
+cors = CORS(app)
+app.register_blueprint(candidato_blueprint)
+app.register_blueprint(mesa_blueprint)
+app.register_blueprint(partido_blueprint)
+app.register_blueprint(resultado_blueprint)
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+@app.route("/", methods=['GET'])
+def home():
+    response = {"message": "Bienvenido a la pagina de la registraduria"}
+    return jsonify(response)
 
 
-# Press the green button in the gutter to run the script.
+# configuracion y ejecucion de la app
+
+def load_file_config():
+    with open("config.json") as file:
+        data = json.load(file)
+    return data
+
+
 if __name__ == '__main__':
-    print_hi('Jonathan')
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+    data_config = load_file_config()
+    print("Server running: http://" + data_config.get('url-backend') + ":" + str(data_config.get('port')))
+    serve(app, host=data_config.get('url-backend'), port=data_config.get('port'))
